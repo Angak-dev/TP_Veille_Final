0ea380395cf81bf121eeb2c7507aa8d429535561
Mathieu Bourassa <mb@p3f.ca>
2020-04-19 10:22:07 -0400

    Ajout du dossier et fichier mjs

--- functions.php
+++ functions.php
@@ -1,27 +1,5 @@
 <?php
 
-/* 
- Permet d'adapter la requête principale avant qu'elle
- ne s'exécute.
-- La liste de conférences pourra s'afficher entièrement sur un page
-- Elle sera triée selon la date des conférences
-- En ordre ascendant
- */
-/*function extraire_evenements( $query ) {
-    if (! is_front_page() && $query->is_category('evenement'))
-    {
-       $query->set( 'posts_per_page', -1 );
-       $query->set( 'orderby', 'date' );
-       $query->set( 'order', 'asc' );
-    }
- }
- add_action( 'pre_get_posts', 'extraire_evenements' );*/
-
-
-
-
-
-
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 function my_theme_enqueue_styles() {
  
@@ -32,7 +10,7 @@ function my_theme_enqueue_styles() {
         get_stylesheet_directory_uri() . '/style.css',
         array( $parent_style ),
         wp_get_theme()->get('Version'));
-    wp_enqueue_script( 'main_js',get_stylesheet_directory_uri() . '/js/main.js', NULL, 1.0,true);
+    wp_enqueue_script( 'main_js', get_stylesheet_directory_uri() . '/mjs/main.js', NULL, 1.0, true);
 }
 
 
